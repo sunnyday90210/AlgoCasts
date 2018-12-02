40e531688da47bad7b7863d0dcd4dc17089f73e8
Robert Wollny <robertjohnwollny@gmail.com>
2018-12-01 23:22:43 -0800

    :tada:

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -9,3 +9,34 @@
 function reverse(str) {}
 
 module.exports = reverse;
+
+// Easy solution with arry revers
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// For of solution
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Reduce method to solve the problem
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
+
+// One line solution with arrow function function reverse(str) {
+// return str.split('').reduce((rev, char) => char + rev);
+// }(params) {
+
+// }
